e4cbfeead956f6aa864eda3068c9daba1e2f0993
MatteoBassanin <matteo.bassanin@gmail.com>
2023-05-24 16:18:04 +0200

    created new model and seeder, added column type_id

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Type;
+use Faker\Generator as Faker;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++) {
+
+            $newType = new Type();
+            $newType->name = $faker->word();
+            $newType->slug = Str::slug($newType->name, '-');
+            $newType->save();
+
+        }
+    }
+}
